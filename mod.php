5da53e9383dbbf1f9dde4bb732d1cf3ca6642c92
LsHallo <xlshallox@gmail.com>
2020-09-15 20:31:49 +0200

    slight changes to proxy php

--- mod.php
+++ mod.php
@@ -1,4 +1,5 @@
 <?php
+    //Factorio mod portal API proxy since it has Access-Control-Allow-Origin not set.
     $allowed = ['Advanced-Electric-Revamped-v16', 'LogisticStatus'];
     $mod_name = $_GET['name'];
     if(in_array($mod_name, $allowed)) {
@@ -6,7 +7,7 @@
         $page = json_decode($page, true);
         http_response_code(200);
         header('Content-Type: application/json');
-        header('Access-Control-Allow-Origin: lshallo.eu');
+        header('Access-Control-Allow-Origin: https://lshallo.eu');
         echo json_encode($page['downloads_count']);
     } else {
         http_response_code(403);
