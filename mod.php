4d06f0da0b469cffd1bc5632079afe4bf7e49c22
LsHallo <xlshallox@gmail.com>
2020-10-29 18:38:49 +0100

    api update

--- mod.php
+++ mod.php
@@ -3,7 +3,9 @@
     $allowed = ['Advanced-Electric-Revamped-v16', 'LogisticStatus'];
     $mod_name = $_GET['name'];
     if(in_array($mod_name, $allowed)) {
-        $page = file_get_contents("https://mods.factorio.com/api/mods/${mod_name}");
+        //Use curl since file_get_contents would not terminate in php-fpm
+        //$page = file_get_contents("https://mods.factorio.com/api/mods/${mod_name}");
+        $page = curlGet("https://mods.factorio.com/api/mods/${mod_name}");
         $page = json_decode($page, true);
         http_response_code(200);
         header('Content-Type: application/json');
@@ -12,4 +14,20 @@
     } else {
         http_response_code(403);
         echo "Unauthorized";
+    }
+
+    function curlGet($url) {
+        //  Initiate curl
+        $ch = curl_init();
+        // Will return the response, if false it print the response
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        // Set the url
+        curl_setopt($ch, CURLOPT_URL, $url);
+        // Execute
+        $result=curl_exec($ch);
+        // Closing
+        curl_close($ch);
+        return $result;
     }
